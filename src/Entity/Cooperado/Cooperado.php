87bb446e8ac54f7ad83328da818e12034d34120f
Victor Moraes <victor.moraes@outlook.com.br>
2019-08-06 18:25:10 -0300

    novo commit

--- src/Entity/Cooperado/Cooperado.php
+++ src/Entity/Cooperado/Cooperado.php
@@ -4,282 +4,623 @@
 
     use Doctrine\ORM\Mapping as ORM;
 
+    /**
+     * ORM\Entity(repositoryClass="App\Repository\CooperadoRepository")
+     */
     class Cooperado
     {
+        /**
+         * @ORM\Id()
+         * @ORM\GeneratedValue()
+         * @ORM\Column(type="integer")
+         */
         private $id;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=100)
+         */
         private $nome;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=20)
+         */
         private $sexo;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=20)
+         */
         private $estadoCivil;
+
+        /**
+         * @var date
+         * @ORM\Column(type="date")
+         */
         private $dataNascimento;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=20)
+         */
         private $racaCor;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=20)
+         */
         private $necessidadeEspecial;
 
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=15)
+         */
         private $cpf;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=15)
+         */
         private $rg;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=10)
+         */
         private $orgaoExpedidorRg;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=10)
+         */
         private $dataExpedicaoRg;
         
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=20)
+         */
         private $naturalidade;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=20)
+         */
         private $nacionalidade;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=100)
+         */
         private $nomePai;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=100)
+         */
         private $nomeMae;
+
+        /**
+         * @var float
+         * @ORM\Column(type="float")
+         */
         private $rendaFamiliar;
 
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=20)
+         */
         private $matricula;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=100)
+         */
         private $curso;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=10)
+         */
         private $turma;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=20)
+         */
         private $escolaridade;
         
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=100)
+         */
         private $rua;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=100)
+         */
         private $bairro;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=50)
+         */
         private $cidade;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=10)
+         */
         private $cep;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=20)
+         */
         private $estado;
         
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=15)
+         */
         private $telefoneResidencial;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=15)
+         */
         private $telefoneCelular;
+
+        /**
+         * @var string
+         * @ORM\Column(type="string", length=100)
+         */
         private $email;
+
+        /**
+         * @var date
+         * @ORM\Column(type="date")
+         */
         private $dataMatricula;
+
+        /**
+         * @var float
+         * @ORM\Column(type="float")
+         */
         private $cotaParte;
 
+        // Geters & Seters
+
+        /**
+         * @return mixed
+         */
         public function getId()
         {
             return $this->id;
         }
 
+        /**
+         * @param mixed $id
+         * @return Cooperado
+         */
         public function setId($id)
         {
             $this->id = $id;
+            return $this;
         }
 
-        public function getNome()
+        /**
+         * @return string
+         */
+        public function getNome(): string
         {
             return $this->nome;
         }
 
-        public function setNome($nome)
+        /**
+         * @param string $nome
+         * @return Cooperado
+         */
+        public function setNome($nome): Cooperado
         {
             $this->nome = $nome;
+            return $this;
         }
 
-        public function getSexo()
+        /**
+         * @return string
+         */
+        public function getSexo(): string
         {
             return $this->sexo;
         }
 
-        public function setSexo($sexo)
+        /**
+         * @param string $sexo
+         * @return Cooperado
+         */
+        public function setSexo($sexo): Cooperado
         {
             $this->sexo = $sexo;
+            return $this;
         }
 
-        public function getEstadoCivil()
+        /**
+         * @return string
+         */
+        public function getEstadoCivil(): string
         {
             return $this->estadoCivil;
         }
 
-        public function setEstadoCivil($estadoCivil)
+        /**
+         * @param string $estadoCivil
+         * @return Cooperado
+         */
+        public function setEstadoCivil($estadoCivil): Cooperado
         {
             $this->estadoCivil = $estadoCivil;
+            return $this;
         }
 
-        public function getDataNascimento()
+        /**
+         * @return date
+         */
+        public function getDataNascimento(): date
         {
             return $this->dataNascimento;
         }
 
-        public function setDataNascimento($dataNascimento)
+        /**
+         * @param date $dataNascimento
+         * @return Cooperado
+         */
+        public function setDataNascimento($dataNascimento): Cooperado
         {
             $this->dataNascimento = $dataNascimento;
+            return $this;
         }
 
-        public function getRacaCor()
+        /**
+         * @return string
+         */
+        public function getRacaCor(): string
         {
             return $this->racaCor;
         }
 
-        public function setRacaCor($racaCor)
+        /**
+         * @param string $racaCor
+         * @return Cooperado
+         */
+        public function setRacaCor($racaCor): Cooperado
         {
             $this->racaCor = $racaCor;
+            return $this;
         }
 
-        public function getNecessidadeEspecial()
+        /**
+         * @return string
+         */
+        public function getNecessidadeEspecial(): string
         {
             return $this->necessidadeEspecial;
         }
 
-        public function setNecessidadeEspecial($necessidadeEspecial)
+        /**
+         * @param string $necessidadeEspecial
+         * @return Cooperado
+         */
+        public function setNecessidadeEspecial($necessidadeEspecial): Cooperado
         {
             $this->necessidadeEspecial = $necessidadeEspecial;
+            return $this;
         }
 
-        public function getCpf()
+        /**
+         * @return string
+         */
+        public function getCpf(): string
         {
             return $this->cpf;
         }
 
-        public function setCpf($cpf)
+        /**
+         * @param string $cpf
+         * @return Cooperado
+         */
+        public function setCpf($cpf): Cooperado
         {
             $this->cpf = $cpf;
+            return $this;
         }
 
-        public function getRg()
+        /**
+         * @return string
+         */
+        public function getRg(): string
         {
             return $this->rg;
         }
 
-        public function setRg($rg)
+        /**
+         * @param string $rg
+         * @return Cooperado
+         */
+        public function setRg($rg): Cooperado
         {
             $this->rg = $rg;
+            return $this;
         }
 
-        public function getOrgaoExpedidorRg()
+        /**
+         * @return string
+         */
+        public function getOrgaoExpedidorRg(): string
         {
             return $this->orgaoExpedidorRg;
         }
 
-        public function setOrgaoExpedidorRg($orgaoExpedidorRg)
+        /**
+         * @param string $orgaoExpedidorRg
+         * @return Cooperado
+         */
+        public function setOrgaoExpedidorRg($orgaoExpedidorRg): Cooperado
         {
             $this->orgaoExpedidorRg = $orgaoExpedidorRg;
+            return $this;
         }
 
-        public function getDataExpedicaoRg()
+        /**
+         * @return date
+         */
+        public function getDataExpedicaoRg(): date
         {
             return $this->dataExpedicaoRg;
         }
 
-        public function setDataExpedicaoRg($dataExpedicaoRg)
+        /**
+         * @param date $dataExpedicaoRg
+         */
+        public function setDataExpedicaoRg($dataExpedicaoRg): Cooperado
         {
             $this->dataExpedicaoRg = $dataExpedicaoRg;
+            return $this;
         }
 
-        public function getNaturalidade()
+        /**
+         * @return string
+         */
+        public function getNaturalidade(): string
         {
             return $this->naturalidade;
         }
 
-        public function setNaturalidade($naturalidade)
+        /**
+         * @param string $naturalidade
+         * @return Cooperado
+         */
+        public function setNaturalidade($naturalidade): Cooperado
         {
             $this->naturalidade = $naturalidade;
+            return $this;
         }
 
-        public function getNacionalidade()
+        /**
+         * @return string
+         */
+        public function getNacionalidade(): string
         {
             return $this->nacionalidade;
         }
 
-        public function setNacionalidade($nacionalidade)
+        /**
+         * @param string $nacionalidade
+         * @return Cooperado
+         */
+        public function setNacionalidade($nacionalidade): Cooperado
         {
             $this->nacionalidade = $nacionalidade;
+            return $this;
         }
 
-        public function getNomePai()
+        /**
+         * @return string
+         */
+        public function getNomePai(): string
         {
             return $this->nomePai;
         }
 
-        public function setNomePai($nomePai)
+        /**
+         * @param string $nomePai
+         * @return Cooperado
+         */
+        public function setNomePai($nomePai): Cooperado
         {
             $this->nomePai = $nomePai;
+            return $this;
         }
 
-        public function getNomeMae()
+        /**
+         * @return string
+         */
+        public function getNomeMae(): string
         {
             return $this->nomeMae;
         }
 
-        public function setNomeMae($nomeMae)
+        /**
+         * @param string $nomeMae
+         * @return Cooperado
+         */
+        public function setNomeMae($nomeMae): Cooperado
         {
             $this->nomeMae = $nomeMae;
+            return $this;
         }
 
-        public function getRendaFamiliar()
+        /**
+         * @return float
+         */
+        public function getRendaFamiliar(): float
         {
             return $this->rendaFamiliar;
         }
 
-        public function setRendaFamiliar($rendaFamiliar)
+        /**
+         * @param float $rendaFamiliar
+         * @return Cooperado
+         */
+        public function setRendaFamiliar($rendaFamiliar): Cooperado
         {
             $this->rendaFamiliar = $rendaFamiliar;
+            return $this;
         }
 
-        public function getMatricula()
+        /**
+         * @return string
+         */
+        public function getMatricula(): string
         {
             return $this->matricula;
         }
 
-        public function setMatricula($matricula)
+        /**
+         * @param string $matricula
+         * @return Cooperado
+         */
+        public function setMatricula($matricula): Cooperado
         {
             $this->matricula = $matricula;
+            return $this;
         }
 
-        public function getCurso()
+        /**
+         * @return string
+         */
+        public function getCurso(): string
         {
             return $this->curso;
         }
 
-        public function setCurso($curso)
+        /**
+         * @param string $curso
+         * @return Cooperado
+         */
+        public function setCurso($curso): Cooperado
         {
             $this->curso = $curso;
+            return $this;
         }
 
-        public function getTurma()
+        /**
+         * @return string
+         */
+        public function getTurma(): string
         {
             return $this->turma;
         }
 
-        public function setTurma($turma)
+        /**
+         * @param string $turma
+         * @return Cooperado
+         */
+        public function setTurma($turma): Cooperado
         {
             $this->turma = $turma;
+            return $this;
         }
 
-        public function getEscolaridade()
+        /**
+         * @return string
+         */
+        public function getEscolaridade(): string
         {
             return $this->escolaridade;
         }
 
-        public function setEscolaridade($escolaridade)
+        /**
+         * @param string $escolaridade
+         * @return Cooperado
+         */
+        public function setEscolaridade($escolaridade): Cooperado
         {
             $this->escolaridade = $escolaridade;
+            return $this;
         }
 
-        public function getRua()
+        /**
+         * @return string
+         */
+        public function getRua(): string
         {
             return $this->rua;
         }
 
-        public function setRua($rua)
+        /**
+         * @param string $rua
+         * @return Cooperado
+         */
+        public function setRua($rua): Cooperado
         {
             $this->rua = $rua;
+            return $this;
         }
 
-        public function getBairro()
+        /**
+         * @return string
+         */
+        public function getBairro(): string
         {
             return $this->bairro;
         }
 
-        public function setBairro($bairro)
+        /**
+         * @param string $bairro
+         * @return Cooperado
+         */
+        public function setBairro($bairro): Cooperado
         {
             $this->bairro = $bairro;
+            return $this;
         }
 
-        public function getCidade()
+        /**
+         * @return string
+         */
+        public function getCidade(): string
         {
             return $this->cidade;
         }
 
-        public function setCidade($cidade)
+        /**
+         * @param string $cidade
+         * @return Cooperado
+         */
+        public function setCidade($cidade): Cooperado
         {
             $this->cidade = $cidade;
+            return $this;
         }
 
-        public function getCep()
+        /**
+         * @return string
+         */
+        public function getCep(): string
         {
             return $this->cep;
         }
 
-        public function setCep($cep)
+        /**
+         * @param string $cep
+         * @return Cooperado
+         */
+        public function setCep($cep): Cooperado
         {
             $this->cep = $cep;
+            return $this;
         }
 
         public function getEstado()
