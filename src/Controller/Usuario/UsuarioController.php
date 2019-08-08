2e3d6c5a1f12e43089cf1b02fccd8a4e0e47a955
Victor Moraes <victor.moraes@outlook.com.br>
2019-08-08 09:23:44 -0300

    criando classe de entidade usuario

--- src/Controller/Usuario/UsuarioController.php
+++ src/Controller/Usuario/UsuarioController.php
@@ -1,25 +1,36 @@
 <?php
 
-    namespace App\Controller\Usuario;
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+namespace App\Controller\Usuario;
 
-    use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
-    class UsuarioController extends AbstractController
+class UsuarioController extends AbstractController
+{
+    /**
+     * @Route("/usuario", name="usuario")
+     */
+    public function index()
     {
-        /**
-         * Retorna a view principal para gerenciamento de usuários
-         */
-        public function usuario()
-        {
-            return $this->render('usuario/usuario.html.twig');
-        }
-
-        /**
-         * Retorna a view para login no sistema
-         */
-        public function loginUsuario()
-        {
-            return $this->render('usuario/login.html.twig');
-        }
+        return $this->render('usuario/index.html.twig', [
+            'controller_name' => 'UsuarioController',
+        ]);
     }
+
+    /**
+     * Retorna a view principal para gerenciamento de usuários
+     */
+    public function usuario()
+    {
+        return $this->render('usuario/usuario.html.twig');
+    }
+    
+    /**
+     * Retorna a view para login no sistema
+     */
+    public function loginUsuario()
+    {
+        return $this->render('usuario/login.html.twig');
+    }
+}
---
+++ src/Entity/Usuario/Usuario.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UsuarioRepository")
+ */
+class Usuario
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $login;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $senha;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getSenha(): ?string
+    {
+        return $this->senha;
+    }
+
+    public function setSenha(string $senha): self
+    {
+        $this->senha = $senha;
+
+        return $this;
+    }
+}
