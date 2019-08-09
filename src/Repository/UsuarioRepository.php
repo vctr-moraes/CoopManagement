8b835bfcb03f82588eccde1c0ffad256ceca0f5a
Victor Moraes <victor.moraes@outlook.com.br>
2019-08-09 18:15:22 -0300

    criada a classe Cooperado

--- src/Repository/UsuarioRepository.php
+++ src/Repository/UsuarioRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\Usuario;
+use App\Entity\Usuario\Usuario;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
