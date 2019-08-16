53fca08a2dc711f7d04e05b84f6257ef5f202612
vctr-moraes <victor.moraes@outlook.com.br>
2019-08-16 00:18:46 -0300

    novo commit

--- src/Repository/ServicoRepository.php
+++ src/Repository/ServicoRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\Servico;
+use App\Entity\Servico\Servico;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
