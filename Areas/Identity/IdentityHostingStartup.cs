b06c9db63fd1a4caea5e40deaebe3197a220ff52
Victor Moraes <49352927+vctr-moraes@users.noreply.github.com>
2019-12-19 17:48:59 -0300

    Configuração de scaffold para identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CoopManagement.Areas.Identity.Data;
+using CoopManagement.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CoopManagement.Areas.Identity.IdentityHostingStartup))]
+namespace CoopManagement.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CoopManagementContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CoopManagementContextConnection")));
+
+                services.AddDefaultIdentity<CoopManagementUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<CoopManagementContext>();
+            });
+        }
+    }
+}
